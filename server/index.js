be430c63ec9b5e0065a73a9b9ac923aa14e9f22c
gowtham <sgowtham.konijeti@gmail.com>
2026-03-08 19:45:33 +0530

    CORS Updated 2

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors({
     origin:[
-        'https://printhub-sigma.vercel.app/'
+        'https://printhub-sigma.vercel.app'
     ],
     credentials: true
 }))
